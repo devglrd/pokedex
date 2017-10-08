c570d29eb0a5c6a6a4e0e9fdccdb41bc2cd33aa7
Remi Guillard <glrd.remi@gmail.com>
2017-10-08 21:46:30 +0200

    first commit

---
+++ pokedex3/Model/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by Remi Guillard on 08/10/2017.
+//  Copyright © 2017 Remi Guillard. All rights reserved.
+//
+
+import Foundation
---
+++ pokedex3/View/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCellCollectionViewCell.swift
+//  pokedex3
+//
+//  Created by Remi Guillard on 08/10/2017.
+//  Copyright © 2017 Remi Guillard. All rights reserved.
+//
+
+import UIKit
+
+class PokeCellCollectionViewCell: UICollectionViewCell {
+    
+}
